fe44ff9852446e8f58d4a811772db5b8a33d096c
baradmitrij <baradmitrij@yandex.ru>
2024-09-19 02:56:07 +0300

    feat: change "item of Todo list" to "item of Good list"

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -1,49 +1,23 @@
-import React, { useState } from 'react';
+import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
-import { plural } from '../../utils';
 import './style.css';
+import { ACTION } from '../../store';
+import { bem } from '../../utils';
 
-function Item(rawProps) {
-  const props = {
-    item: rawProps.item,
-    onDelete: rawProps.onDelete ? rawProps.onDelete : () => {},
-    onSelect: rawProps.onSelect ? rawProps.onSelect : () => {},
-  };
+function Item({ item }) {
+  const addGoodToCart = useCallback(() => {
+    ACTION.addGoodToCart(item.code);
+  }, []);
 
-  // Счётчик выделений
-  const [count, setCount] = useState(0);
-
-  const callbacks = {
-    onClick: () => {
-      props.onSelect(props.item.code);
-      if (!props.item.selected) {
-        setCount(count + 1);
-      }
-    },
-    onDelete: e => {
-      e.stopPropagation();
-      props.onDelete(props.item.code);
-    },
-  };
+  const cn = bem('Item');
 
   return (
-    <div
-      className={'Item' + (props.item.selected ? ' Item_selected' : '')}
-      onClick={callbacks.onClick}
-    >
-      <div className="Item-code">{props.item.code}</div>
-      <div className="Item-title">
-        {props.item.title}{' '}
-        {count
-          ? ` | Выделяли ${count} ${plural(count, {
-              one: 'раз',
-              few: 'раза',
-              many: 'раз',
-            })}`
-          : ''}
-      </div>
-      <div className="Item-actions">
-        <button onClick={callbacks.onDelete}>Удалить</button>
+    <div className={cn()}>
+      <div className={cn('code')}>{item.code}</div>
+      <div className={cn('title')}>{item.title}</div>
+      <div className={cn('price')}>{item.price}</div>
+      <div className={cn('actions')}>
+        <button onClick={addGoodToCart}>Добавить</button>
       </div>
     </div>
   );
@@ -53,11 +27,8 @@ Item.propTypes = {
   item: PropTypes.shape({
     code: PropTypes.number,
     title: PropTypes.string,
-    selected: PropTypes.bool,
-    count: PropTypes.number,
+    price: PropTypes.number,
   }).isRequired,
-  onDelete: PropTypes.func,
-  onSelect: PropTypes.func,
 };
 
 export default React.memo(Item);
