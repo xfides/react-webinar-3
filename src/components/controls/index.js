11d112ddc2594167c1a12a0912dca56bcb564681
baradmitrij <baradmitrij@yandex.ru>
2024-09-19 04:24:22 +0300

    refactor: change Controls component (BEM + markUp + logic)

--- src/components/controls/index.js
+++ src/components/controls/index.js
@@ -1,21 +1,37 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import './style.css';
+import { ACTION } from '../../store';
+import { bem, plural } from '../../utils';
 
-function Controls(rawProps) {
-  const props = {
-    onAdd: rawProps.onAdd ? rawProps.onAdd : () => {},
-  };
+function Controls() {
+  const cartInfo = ACTION.buildCartInfo();
+  const countOfGoods = cartInfo.length;
+  const totalCost = cartInfo.reduce((totalCost, oneCartGoodInfo) => {
+    return totalCost + oneCartGoodInfo.count * oneCartGoodInfo.price;
+  }, 0);
+
+  const normalizeWord = plural(countOfGoods, {
+    one: 'товар',
+    few: 'товара',
+    many: 'товаров',
+  });
+
+  let shortCartInfoStr = countOfGoods
+    ? `${countOfGoods} ${normalizeWord} / ${totalCost} Р`
+    : 'пусто';
+
+  const cn = bem('Controls');
 
   return (
-    <div className="Controls">
-      <button onClick={props.onAdd}>Добавить</button>
+    <div className={cn()}>
+      <div className={cn('shortCartInfo')}>
+        {`В корзине: `} <strong>{shortCartInfoStr}</strong>
+      </div>
+      <div className={cn('actions')}>
+        <button onClick={ACTION.toggleModal}>Перейти</button>
+      </div>
     </div>
   );
 }
 
-Controls.propTypes = {
-  onAdd: PropTypes.func,
-};
-
-export default React.memo(Controls);
+export default Controls;
