2d737f41c1cc660147b4be569d86e65b208d0de8
baradmitrij <baradmitrij@yandex.ru>
2024-09-28 22:57:53 +0300

    feat: improve basket-tool with Link on main page

--- src/components/basket-tool/index.js
+++ src/components/basket-tool/index.js
@@ -3,22 +3,29 @@ import PropTypes from 'prop-types';
 import { cn as bem } from '@bem-react/classname';
 import { numberFormat, plural } from '../../utils';
 import './style.css';
+import { Link } from 'react-router-dom';
 
 function BasketTool({ sum = 0, amount = 0, onOpen = () => {} }) {
   const cn = bem('BasketTool');
   return (
     <div className={cn()}>
-      <span className={cn('label')}>В корзине:</span>
-      <span className={cn('total')}>
-        {amount
-          ? `${amount} ${plural(amount, {
-              one: 'товар',
-              few: 'товара',
-              many: 'товаров',
-            })} / ${numberFormat(sum)} ₽`
-          : `пусто`}
-      </span>
-      <button onClick={onOpen}>Перейти</button>
+      <div className={cn('nav')}>
+        <Link to={'/main'}>Главная</Link>
+      </div>
+
+      <div className={cn('info')}>
+        <span className={cn('label')}>В корзине:</span>
+        <span className={cn('total')}>
+          {amount
+            ? `${amount} ${plural(amount, {
+                one: 'товар',
+                few: 'товара',
+                many: 'товаров',
+              })} / ${numberFormat(sum)} ₽`
+            : `пусто`}
+        </span>
+        <button onClick={onOpen}>Перейти</button>
+      </div>
     </div>
   );
 }
