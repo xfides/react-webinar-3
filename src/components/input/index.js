c34c575e931981af4d743c1083d58e9abef90444
baradmitrij <baradmitrij@yandex.ru>
2024-10-06 20:28:27 +0300

    refactor: add small improvements to Input Component

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -9,9 +9,10 @@ function Input(props) {
   const propsOnChange = props.onChange ?? (() => {});
   const type = props.type ?? 'text';
   const theme = props.theme ?? '';
+  const propsValue = props.value ?? '';
 
   // Внутренний стейт для быстрого отображения ввода
-  const [value, setValue] = useState(props.value);
+  const [value, setValue] = useState(propsValue);
 
   const onChangeDebounce = useCallback(
     debounce(value => propsOnChange(value, props.name), 600),
@@ -25,12 +26,13 @@ function Input(props) {
   };
 
   // Обновление стейта, если передан новый value
-  useLayoutEffect(() => setValue(props.value), [props.value]);
+  useLayoutEffect(() => setValue(propsValue), [propsValue]);
 
   const cn = bem('Input');
   return (
     <input
       className={cn({ theme: theme })}
+      name={props.name}
       value={value}
       type={type}
       placeholder={props.placeholder}
