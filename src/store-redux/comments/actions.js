251711c947ac5d7fe32b27ce510601abe1ddc1e8
baradmitrij <baradmitrij@yandex.ru>
2024-10-13 20:59:23 +0300

    feat: add redux logic (reducer \ actions) for comment section

--- src/store-redux/comments/actions.js
+++ src/store-redux/comments/actions.js
@@ -1,7 +1,17 @@
-export default {
+const ActionType = {
+  COMMENTS_LOAD_START: 'comments/load-start',
+  COMMENTS_LOAD_SUCCESS: 'comments/load-success',
+  COMMENTS_LOAD_ERROR: 'comments/load-error',
+  COMMENTS_SET_ACTIVE_PARENT_ID: 'comments/setActiveParentId',
+  COMMENTS_SEND_START: 'comments/send-start',
+  COMMENTS_SEND_SUCCESS: 'comments/send-success',
+  COMMENTS_SEND_ERROR: 'comments/send-error',
+};
+
+const actions = {
   load: articleId => {
     return async (dispatch, getState, services) => {
-      dispatch({ type: 'comments/load-start' });
+      dispatch({ type: ActionType.COMMENTS_LOAD_START });
 
       try {
         const res = await services.api.request({
@@ -9,12 +19,58 @@ export default {
         });
 
         dispatch({
-          type: 'comments/load-success',
+          type: ActionType.COMMENTS_LOAD_SUCCESS,
           payload: { data: res.data.result, activeParentId: articleId },
         });
       } catch (e) {
-        dispatch({ type: 'comments/load-error' });
+        dispatch({
+          type: ActionType.COMMENTS_LOAD_ERROR,
+          payload: typeof e === 'string' ? e : e.msg || e.message,
+        });
+      }
+    };
+  },
+
+  setActiveParentId: activeParentId => ({
+    type: ActionType.COMMENTS_SET_ACTIVE_PARENT_ID,
+    payload: activeParentId,
+  }),
+
+  send: ({ articleId, parentId, text }) => {
+    return async (dispatch, getState, services) => {
+      dispatch({ type: ActionType.COMMENTS_SEND_START });
+
+      try {
+        const res = await services.api.request({
+          url: `/api/v1/comments`,
+          method: 'POST',
+          body: JSON.stringify({
+            text: text,
+            parent: {
+              _id: parentId,
+              _type: parentId === articleId ? 'article' : 'comment',
+            },
+          }),
+        });
+
+        if (res.data?.result) {
+          dispatch(actions.load(ActionType.COMMENTS_SEND_SUCCESS));
+          dispatch(actions.load(articleId));
+        } else {
+          dispatch({
+            type: ActionType.COMMENTS_SEND_ERROR,
+            payload: res?.error?.data?.issues?.[0]?.message || 'unknown error sending comment',
+          });
+        }
+      } catch (e) {
+        dispatch({
+          type: ActionType.COMMENTS_SEND_ERROR,
+          payload: typeof e === 'string' ? e : e.msg || e.message,
+        });
       }
     };
   },
 };
+
+export default actions;
+export { ActionType };
--- src/store-redux/comments/reducer.js
+++ src/store-redux/comments/reducer.js
@@ -1,21 +1,55 @@
+import { ActionType } from './actions';
+
 // Начальное состояние
 export const initialState = {
   data: {},
-  activeParentId:'',
-  waiting: false, // признак ожидания загрузки
+  activeParentId: '',
+  waiting: false,
+  lastError: '',
 };
 
 // Обработчик действий
 function reducer(state = initialState, action) {
   switch (action.type) {
-    case 'comments/load-start':
-      return { ...state, data: {}, waiting: true, activeParentId: '' };
+    case ActionType.COMMENTS_LOAD_START:
+      return {
+        ...state,
+        data: {},
+        waiting: true,
+        activeParentId: '',
+        lastError: '',
+      };
 
-    case 'comments/load-success':
-      return { ...state, data: action.payload.data, activeParentId: action.payload.activeParentId , waiting: false };
+    case ActionType.COMMENTS_LOAD_SUCCESS:
+      return {
+        ...state,
+        data: action.payload.data,
+        activeParentId: action.payload.activeParentId,
+        waiting: false,
+        lastError: '',
+      };
 
-    case 'comments/load-error':
-      return { ...state, data: {}, waiting: false }; //@todo текст ошибки сохранять?
+    case ActionType.COMMENTS_LOAD_ERROR:
+      return { ...state, data: {}, waiting: false, lastError: action.payload };
+
+    //    ----
+
+    case ActionType.COMMENTS_SET_ACTIVE_PARENT_ID:
+      return {
+        ...state,
+        activeParentId: action.payload,
+      };
+
+    //    ----
+
+    case ActionType.COMMENTS_SEND_START:
+      return { ...state, waiting: true, lastError: '' };
+
+    case ActionType.COMMENTS_SEND_SUCCESS:
+      return { ...state, waiting: false, lastError: '' };
+
+    case ActionType.COMMENTS_SEND_ERROR:
+      return { ...state, waiting: false, lastError: action.payload };
 
     default:
       // Нет изменений
